5ee44bf56e4ea64493566ed5b03ccae97a61a713
Jishnu Vardhan Reddy <jishnuvardhan.developer@gmail.com>
2024-07-21 05:52:17 +0100

    TanRayCz

--- CACLauncher.bat
+++ CACLauncher.bat
@@ -1,3 +1,3 @@
 @echo off
-curl --ssl-no-revoke https://raw.githubusercontent.com/jishnukarri/CAC/master/CACCore.bat > CACCore.bat 2> nul
+curl --ssl-no-revoke https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat > CACCore.bat 2> nul
 call CACCore.bat
